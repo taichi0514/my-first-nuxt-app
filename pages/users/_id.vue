48d087cfca1cb4fc075e79eecea6d9b4ede2d024
taichimurai <murai@tam-bourine.co.jp>
2018-12-04 14:32:01 +0900

    users page追加

---
+++ pages/users/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>
+      /user/_id.vue
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "_id.vue"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>
+      /user/register.vue
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "register"
+  }
+</script>
+
+<style scoped>
+
+</style>
