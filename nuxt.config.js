18d591f04ae200219cf8687893efbd25ca99e4bc
taichi.murai <taichimurai0514@gmail.com>
2018-12-17 00:03:21 +0900

    seo 追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,19 +4,20 @@ module.exports = {
   */
   head: {
     title: 'my-first-nuxt-app',
+    titleTemplate: '%s | Nuxt.js tag item viewer',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: {color: '#3B8070'},
   modules: [
     '@nuxtjs/axios'
   ],
@@ -34,7 +35,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, {isDev, isClient}) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
